6a5c0744841a80e7a0527af2ce73953152b1d9e9
jdrichardstech <jdrichardstech@gmail.com>
2016-03-30 14:49:00 -0400

    update sidebar links

--- public/app/components/Sidebar.js
+++ public/app/components/Sidebar.js
@@ -10,11 +10,11 @@ var Sidebar = React.createClass({
 	                <div className="container clearfix">
 	                    <div id="primary-menu-trigger"><i className="icon-reorder"></i></div>
 	                    <div id="logo" className="nobottomborder">
-	                        <a href="/" className="standard-logo" data-dark-logo="images/logo-side-dark.png">
+	                        <a href="/" className="standard-logo" data-dark-logo="images/logo-side.png">
 	                        	<img src="images/logo-side.png" alt="Canvas Logo" />
 	                        </a>
-	                        <a href="/" className="retina-logo" data-dark-logo="images/logo-side-dark@2x.png">
-	                        	<img src="images/logo-side@2x.png" alt="Canvas Logo" />
+	                        <a href="/" className="retina-logo" data-dark-logo="images/logo-side.png">
+	                        	<img src="images/logo-side.png" alt="Canvas Logo" />
 	                        </a>
 	                    </div>
 
@@ -26,21 +26,20 @@ var Sidebar = React.createClass({
 	                            <li>
 	                                <a href="/about"><div>About</div></a>
 	                            </li>
-	                            
-	                            <li><a href="/"><div>Pages</div></a>
-	                                <ul>
-	                                    <li><a href="#"><div>One</div></a></li>
-	                                    <li><a href="#"><div>Two</div></a></li>
-	                                    <li><a href="#"><div>Three</div></a></li>
-	                                </ul>
+	                            <li>
+	                                <a href="/deliver"><div>Want To Be A Fetcher?</div></a>
 	                            </li>
+	                            <li>
+	                                <a href="/developer"><div>Meet The Developer</div></a>
+	                            </li>
+	                            
 	                        </ul>
 	                    </nav>
 
 	                    <div className="clearfix visible-md visible-lg">
 	                        <a href="#" className="social-icon si-small si-borderless si-facebook">
-	                            <i className="icon-facebook"></i>
-	                            <i className="icon-facebook"></i>
+	                            <i className="icon-linkedin"></i>
+	                            <i className="icon-linkedin"></i>
 	                        </a>
 
 	                        <a href="#" className="social-icon si-small si-borderless si-twitter">
