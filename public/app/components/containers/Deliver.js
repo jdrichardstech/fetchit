144c39124fa03af458e8e1ec6cd7d362ee5042ed
jdrichardstech <jdrichardstech@gmail.com>
2016-03-30 15:05:05 -0400

    update Deliver page

--- public/app/components/containers/Deliver.js
+++ public/app/components/containers/Deliver.js
@@ -13,19 +13,19 @@ var Deliver = React.createClass({
 
 							<div className="container clearfix">
 
-				<div className="heading-block center nobottomborder bottommargin-lg">
-									<h1>Want To Be A Fetcher?</h1>
-									<span>Specializes in creating Brand Identity for emerging Startups</span>
-				</div>
+					<div className="heading-block center nobottomborder bottommargin-lg">
+										<h1>Want To Be A Fetcher?</h1>
+										<span>Specializes in creating Brand Identity for emerging Startups</span>
+					</div>
 
-				<div className="col_one_third">
-						<div className="feature-box fbox-center fbox-effect">
-							<div className="fbox-icon">
-								<a href="#"><i className="icon-calendar i-alt"></i></a>
+					<div className="col_one_third">
+							<div className="feature-box fbox-center fbox-effect">
+								<div className="fbox-icon">
+									<a href="#"><i className="icon-calendar i-alt"></i></a>
+								</div>
+								<h3>Responsive Layout</h3>
+								<p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
 							</div>
-							<h3>Responsive Layout</h3>
-							<p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
-						</div>
 					</div>
 
 					<div className="col_one_third">
@@ -50,7 +50,50 @@ var Deliver = React.createClass({
 
 					<div className="clear"></div>
 				
-								
+
+
+					<div className="fancy-title title-bottom-border">
+						<h3>Your Job As A Fetcher</h3>
+					</div>
+
+					<p>Repudiandae quasi perspiciatis ea placeat nobis asperiores quod fuga ipsa facere enim ipsum expedita debitis, sit quia adipisci deserunt vitae hic obcaecati voluptates rerum nihil.</p>
+
+					<div className="accordion accordion-bg clearfix">
+
+						<div className="acctitle"><i className="acc-closed icon-ok-circle"></i><i className="acc-open icon-remove-circle"></i>Requirements</div>
+						<div className="acc_content clearfix">
+							<ul className="iconlist iconlist-color nobottommargin">
+								<li><i className="icon-ok"></i>B.Tech./ B.E / MCA degree in Computer Science, Engineering or a related stream.</li>
+								<li><i className="icon-ok"></i>3+ years of software development experience.</li>
+								<li><i className="icon-ok"></i>3+ years of Python / Java development projects experience.</li>
+								<li><i className="icon-ok"></i>Minimum of 4 live project roll outs.</li>
+								<li><i className="icon-ok"></i>Experience with third-party libraries and APIs.</li>
+								<li><i className="icon-ok"></i>In depth understanding and experience  of either SDLC or PDLC.</li>
+								<li><i className="icon-ok"></i>Good Communication Skills</li>
+								<li><i className="icon-ok"></i>Team Player</li>
+							</ul>
+						</div>
+
+						<div className="acctitle"><i className="acc-closed icon-ok-circle"></i><i className="acc-open icon-remove-circle"></i>What we Expect from you?
+						</div>
+						<div className="acc_content clearfix">
+							<ul className="iconlist iconlist-color nobottommargin">
+								<li><i className="icon-plus-sign"></i>Design and build applications/ components using open source technology.</li>
+								<li><i className="icon-plus-sign"></i>Taking complete ownership of the deliveries assigned.</li>
+								<li><i className="icon-plus-sign"></i>Collaborate with cross-functional teams to define, design, and ship new features.</li>
+								<li><i className="icon-plus-sign"></i>Work with outside data sources and APIs.</li>
+								<li><i className="icon-plus-sign"></i>Unit-test code for robustness, including edge cases, usability, and general reliability.</li>
+								<li><i className="icon-plus-sign"></i>Work on bug fixing and improving application performance.</li>
+							</ul>
+						</div>
+
+						<div className="acctitle"><i className="acc-closed icon-ok-circle"></i><i className="acc-open icon-remove-circle"></i>What you ve got?</div>
+						<div className="acc_content clearfix">Youll be familiar with agile practices and have a highly technical background, comfortable discussing detailed technical aspects of system design and implementation, whilst remaining business driven. With 5+ years of systems analysis, technical analysis or business analysis experience, youll have an expansive toolkit of communication techniques to enable shared, deep understanding of financial and technical concepts by diverse stakeholders with varying backgrounds and needs. In addition, you will have exposure to financial systems or accounting knowledge.</div>
+
+					</div>
+
+					<a href="#" data-scrollto="#job-apply" className="button button-3d button-black nomargin">Apply Now</a>
+							
 
 							
 							</div>
